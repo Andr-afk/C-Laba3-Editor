9a7167ec20ff0cc047d2ea6406d755401de4b9e9
Andr-afk <56882356+Andr-afk@users.noreply.github.com>
2020-10-11 19:33:29 +0700

    write class 'Editor'. No tested

---
+++ EditorDevelopment/Editor.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorDevelopment
+{
+    class Editor
+    {
+        string number;
+        
+
+        public Editor(string number)
+        {
+            if (number is null) this.number = "0";
+            else this.number = number;
+
+        }
+
+        public string Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+
+
+        //tested!
+        public string ChangeSign()
+        {
+            if (number.Contains('-')) this.number = number.Remove(0, 1);
+            else this.number = '-' + this.number;
+
+            return this.number;
+        }
+
+
+        //tested!
+        public string AddDigit(byte digit)
+        {
+            //функция заменяет число на A, B, C, D, E, F, если оно больше 9
+            string replacer()
+            {
+                if (digit == 0) return this.number;
+                else
+                {
+                    string symbol = Convert.ToString(digit);
+
+                    if (digit > 9)
+                    {
+                        switch (symbol)
+                        {
+                            case "10": symbol = "A"; break;
+                            case "11": symbol = "B"; break;
+                            case "12": symbol = "C"; break;
+                            case "13": symbol = "D"; break;
+                            case "14": symbol = "E"; break;
+                            case "15": symbol = "F"; break;
+                        }
+                    }
+
+                    return symbol;
+                }
+            }
+
+
+            if (this.number == "0") this.number = replacer();
+            else this.number = this.number + replacer();
+
+            return this.number;    
+        }
+
+
+        //tested!
+        public string BS()
+        {
+            if (this.number == "0") return this.number;
+
+            else
+            {
+                return this.number.Remove(number.Length - 1, 1);
+            }
+        }
+
+
+        //tested!
+        public string Clear()
+        {
+            this.number = "0";
+            return this.number;
+        }
+
+
+        //tested!
+        public string SetDot()
+        {
+            if (number.Contains(".")) return this.number;
+            else
+            {
+                this.number = this.number + '.';
+                return this.number;
+            }
+        }
+
+        public string DoEdit(byte i)
+        {
+            if (i >= 0 && i <= 15)  return this.AddDigit(i);
+
+            if (i == 16) return this.SetDot();
+
+            if (i == 17) return this.ChangeSign();
+
+            if (i == 18) return this.BS();
+
+            if (i == 19) return this.Clear();
+
+            return this.number;
+        }
+    }
+}
---
+++ EditorDevelopment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Editor x = new Editor("1234");
+
+            Console.WriteLine(x.SetDot());
+            Console.WriteLine(x.DoEdit(4));
+            Console.WriteLine(x.SetDot());
+            Console.WriteLine(x.ChangeSign());
+            Console.WriteLine(x.AddDigit(12));
+        }
+    }
+}
